27661ec1c74e6935d84bfa42846d572b4be11f48
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-09-15 19:07:05 +0200

    it kinda works but user still sees shortened url when he enters the website instead of seeing dummy text.

--- URL_Shortener/shortener/utilities/short_code.py
+++ URL_Shortener/shortener/utilities/short_code.py
@@ -1,6 +1,7 @@
 from string import ascii_lowercase, ascii_uppercase
 import random
 
+
 def generate_body(length: int) -> str:
     """Function to generate the body (`length` number of letter) consisting
     of random number of both lowercase and uppercase letters.
@@ -11,23 +12,26 @@ def generate_body(length: int) -> str:
     Returns:
         str: body of the short URL
     """
-    
+
     # NOTE: Ensuring that lowercase count does not exceed the total length
     max_lowercase = min(6, length - 1) if length > 1 else 1
-    
+
     no_of_lowercase = random.randint(1, length)
     no_of_uppercase = length - no_of_lowercase
-    
+
     lowercase_chars = [random.choice(ascii_lowercase) for _ in range(no_of_lowercase)]
     uppercase_chars = [random.choice(ascii_uppercase) for _ in range(no_of_uppercase)]
-    
-    body = (lowercase_chars + uppercase_chars)
+
+    body = lowercase_chars + uppercase_chars
     random.shuffle(body)
-    
+
     return "".join(body)
 
 
-def build_short_url(body: str, domain: str, secure: bool) -> str:
+def build_short_url(
+    body: str,
+    domain: str = "smartestnerdfromtown",
+    secure: bool = True,
+) -> str:
     heading = "https://" if secure else "http://"
     return f"{heading}{domain}.me/go/{body}"
-    
--- URL_Shortener/shortener/views.py
+++ URL_Shortener/shortener/views.py
@@ -1,4 +1,28 @@
+import os
+print(os.listdir())
+
+
+
 from django.shortcuts import render
+from .models import Urls, AllUrls
+from .utilities.short_code import (
+    generate_body,
+    build_short_url,
+)
 
 def home(request):
-    return render(request, "shortener/home.html")
+    if request.method == "POST":
+        user_url = request.POST.get("url")
+        body = generate_body(length=8)
+        short_url = build_short_url(body)
+
+        url, creation_status = Urls.objects.get_or_create(
+            original_destination=user_url,
+            generated_short_url=short_url,
+        )
+        unique_url, creation_status = AllUrls.objects.get_or_create(short_url=short_url)
+        
+        return render(request, "shortener/home.html", {"final_url": short_url})
+    
+    elif request.method == "GET":        
+        return render(request, "shortener/home.html", {"final_url": None})
