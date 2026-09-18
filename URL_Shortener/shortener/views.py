55632b7d84843a7081823758dd9b2f29f42fca3a
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-09-18 11:55:17 +0200

    Added helper function for detecting user IP and made  to user whenever user requests POST or GET method

--- URL_Shortener/shortener/views.py
+++ URL_Shortener/shortener/views.py
@@ -1,17 +1,22 @@
-import os
-
-print(os.listdir())
-
+import logging
 from django.shortcuts import render
 from .models import Urls, AllUrls
 from .utilities.short_code import (
     generate_body,
     build_short_url,
 )
+from .security_utilities.utilities import get_client_ip
+
+logger = logging.getLogger(__name__)
+logger.info(f"Logging started successfully from {__name__}")
 
 
 def home(request):
+    USER_IP = get_client_ip(request=request)
+
     if request.method == "POST":
+        logger.info(f"Some user from {USER_IP} IP requested POST method.")
+
         user_url = request.POST.get("url")
         body = generate_body(length=8)
         short_url = build_short_url(body)
@@ -25,4 +30,5 @@ def home(request):
         return render(request, "shortener/home.html", {"final_url": short_url})
 
     elif request.method == "GET":
+        logger.info(f"Some user from {USER_IP} IP visited home page.")
         return render(request, "shortener/home.html", {"final_url": None})
